6750c0ca23a3e17846fcd3f80ad9bf1e6bffc4e4
Léonard <31880546+Leoncocojump@users.noreply.github.com>
2021-01-13 14:03:00 +0100

    Projects Image Change

--- srcipts/projects.js
+++ srcipts/projects.js
@@ -13,3 +13,23 @@ $(".itemNav ul li").click(function () {
     $(".itemNav ul li").removeClass("projectSelect");
     $(this).addClass("projectSelect");
 })
+
+
+// PAGE PROJECTS - Change sources
+
+
+let lists = $(".itemNav ul li");
+
+lists.click(function() {
+    let chapter = $(this).nthParent(2).attr("num");
+    let project = $(this).attr("numProject");
+    console.log(project)
+    $("#ImgProjects").attr('src', 'images/projects/c' + chapter + '/p' + project + '/Img_01.jpg')
+});
+
+$.fn.nthParent = function(n){
+    var p = this;
+    for(var i=0;i<n;i++)
+        p = p.parent();
+    return p;
+}
